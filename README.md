04b3364172c9087d29aa99b543298f0c88583dfc
Saul-Guerrero <saguerreroul@gmail.com>
2023-12-03 11:26:05 -0500

    Inicio el taller 06, se crea la carpeta del punto 1

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1>Taller 06: Saúl Guerrero</h1>
+<hr>
+
+<h2>Información</h2>
+<p>Curso: Full Stack Intermedio</p>
+<p>Profesor: Andres Pineda</p>
+<p>Estudiante: Saúl Guerrero</p>
+
+
+
+<h2>Punto 1: Calculadora operaciones basicas</h2>
+<img src="./public/images/punto-1.png" alt="Calculator">
+<h2>Punto 2: Saber si eres mayor de edad</h2>
+<img src="./public/images/punto-2.png" alt="Age">
+<h2>Punto 3: Enviar datos del usuario a una pagina</h2>
+<img src="./public/images/punto-3.png" alt="page">
+<img src="./public/images/punto-3-1.png" alt="Age">
+<h2>Punto 4: Saber si es mayor de edad con la fecha de nacimiento</h2>
+<img src="./public/images/punto-4.png" alt="Age">
+<h2>Punto 5-6-7: </h2>
+<h3>5 - Base de datos </h3>
+<img src="./public/images/mysql.png" alt="myqsl">
+<h3>6 - Conexion Base de datos </h3>
+<img src="./public/images/conexionBD.png" alt="connection">
+
+<h3>7 - Muestra de datos en pantalla </h3>
+<img src="./public/images/BD.png" alt="connection">
---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>taller 06</title>
+    <link rel="stylesheet" href="./styles/menu-styles.css">
+    <link rel="stylesheet" href="./styles/general-styles.css">
+    
+    
+</head>
+
+<body>
+    <div>
+        <h1>Taller unidad 06</h1>
+        <h2>Saul Jose Guerrero</h2>
+        <nav>
+            <ul>
+                <li>
+                    <p>
+                        Punto 1: Operación de dos números
+                    </p>
+                    <a href="./punto-1">
+                        Punto 1
+                    </a>
+                </li>
+                <li>
+                    <p>
+                        Punto 2: Calculo de Mayoría de Edad
+                    </p>
+                    <a href="./punto-2">
+                        Punto 2 
+                    </a>
+                </li>
+                <li>
+                    <p>
+                        Punto 3: Envio de datos del usuario
+                    </p>
+                    <a href="./punto-3">
+                        Punto 3 
+                    </a>
+                </li>
+                <li>
+                    <p>
+                        Punto 4: Calculo de Mayoría de Edad con Fechas
+                    </p>
+                    <a href="./punto-4">
+                        Punto 4 
+                    </a>
+                </li>
+                <li>
+                    <p>
+                        Punto 5 6: Creación Base de datos y conexion
+                    </p>
+                    <a href="./punto-5-6">
+                        Base de datos
+                    </a>
+                </li>
+                <li>
+                    <p>
+                        Punto 7: Creación programa de filtro
+                    </p>
+                    <a href="./punto-7">
+                        Programa
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</body>
+
+</html>
